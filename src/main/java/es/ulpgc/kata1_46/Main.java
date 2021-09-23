ba7398ab5dab04178bb1dce0876100a290510a07
Baluarte <Baluarte@DESKTOP-P050GBA.localdomain>
2021-09-23 23:36:44 +0200

    He agregado un comentario inutil

--- src/main/java/es/ulpgc/kata1_46/Main.java
+++ src/main/java/es/ulpgc/kata1_46/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         
+        //Este comentario no sirve para nada.
         Person person = new Person("Antonio", new Date(80,0,1));
         System.out.println(person.getName() + " tiene " + person.getAge() + ".");
                 
